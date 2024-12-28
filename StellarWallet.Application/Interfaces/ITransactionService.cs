503782a400157772ec166b5832f67fc24b376e96
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-12-28 02:08:21 -0300

    Refactor error handling to use CustomError type
    Updated ITransactionService and TransactionService to use CustomError instead of DomainError. Modified method signatures and return types accordingly. Introduced _userNotFoundError for handling "User not found" errors. Updated CreateAccount, SendPayment, GetTransaction, GetTestFunds, and GetBalances methods to return appropriate CustomError types for various error scenarios.

--- StellarWallet.Application/Interfaces/ITransactionService.cs
+++ StellarWallet.Application/Interfaces/ITransactionService.cs
@@ -2,16 +2,17 @@
 using StellarWallet.Application.Dtos.Responses;
 using StellarWallet.Domain.Entities;
 using StellarWallet.Domain.Errors;
+using StellarWallet.Domain.Interfaces.Result;
 using StellarWallet.Domain.Result;
 
 namespace StellarWallet.Application.Interfaces
 {
     public interface ITransactionService
     {
-        public Task<Result<BlockchainAccount, DomainError>> CreateAccount(string jwt);
-        public Task<Result<bool, DomainError>> SendPayment(SendPaymentDto sendPaymentDto, string jwt);
-        public Task<Result<TransactionsDto, DomainError>> GetTransaction(string jwt, int pageNumber, int pageSize);
-        public Task<Result<bool, DomainError>> GetTestFunds(string publicKey);
-        public Task<Result<FoundBalancesDto, DomainError>> GetBalances(GetBalancesDto publicKey);
+        public Task<Result<BlockchainAccount, CustomError>> CreateAccount(string jwt);
+        public Task<Result<bool, CustomError>> SendPayment(SendPaymentDto sendPaymentDto, string jwt);
+        public Task<Result<TransactionsDto, CustomError>> GetTransaction(string jwt, int pageNumber, int pageSize);
+        public Task<Result<bool, CustomError>> GetTestFunds(string publicKey);
+        public Task<Result<FoundBalancesDto, CustomError>> GetBalances(GetBalancesDto getBalancesDto);
     }
 }
